04509ec4283e3979e976fbcd8f3e356ac6c7d2cf
Wambologe <thoga92@hotmail.de>
2020-01-18 11:33:42 +0100

    Kommentare

--- Baumstrukturen/GenericOrderedSet.java
+++ Baumstrukturen/GenericOrderedSet.java
@@ -1,11 +1,16 @@
 public interface GenericOrderedSet<E> {
+	// Beum wird nach Schluessel k durchsucht
 	boolean contains(E key);
 	
+	// Kleinster Schluessel
 	E minimum();
 	
+	// Groesster Schluessel
 	E maximum();
 	
+	// Einfuegen des Schluessels k, falls noch nicht vorhanden
 	GenericOrderedSet<E> insert(E key);
 	
+	// Löschen des Schluessels k, falls vorhanden
 	GenericOrderedSet<E> remove(E key);
 }
